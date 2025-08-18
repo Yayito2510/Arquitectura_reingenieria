7cb69a50c2a5c27da25b6e37a1b7453553b4d3aa
Yayito2510 <jjachote@espe.edu.ec>
2025-08-17 22:57:01 -0500

    Subida inicial del proyecto

---
+++ src/main/java/com/example/parquemiento/ParquemientoApplication.java
@@ -0,0 +1,13 @@
+package com.example.parquemiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParquemientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParquemientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/parquemiento/ParquemientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parquemiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParquemientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
